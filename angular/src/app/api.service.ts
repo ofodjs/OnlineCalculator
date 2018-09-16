b130ec52b2591cb64592e347eb3ce8357647b1a8
Sergey Serdyuk <serduksv@gmail.com>
2018-09-16 08:50:07 +0100

    Switched to use calculation data in API request.

--- angular/src/app/api.service.ts
+++ angular/src/app/api.service.ts
@@ -17,13 +17,8 @@ export class ApiService {
 
   constructor(private http: HttpClient) {}
 
-  calculate(x, y, operation): Observable<OperationResult> {
-    const data = {
-      Operand1: x,
-      Operand2: y,
-      MathOperation: operation,
-    };
+  calculate(requestData): Observable<OperationResult> {
     return this.http
-      .post<OperationResult>(`${this.API_URL}/calculate/`, data);
+      .post<OperationResult>(`${this.API_URL}/calculate/`, requestData);
   }
 } 
--- angular/src/app/calculator/calculator.component.ts
+++ angular/src/app/calculator/calculator.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, NgModule} from '@angular/core';
 import {ApiService} from '../api.service';
 import {OperationResult} from '../operationresult';
 import {MathOperations} from '../math.operations';
+import {CalculationData} from '../calculation.data';
 
 @Component({
   selector: 'app-calculator',
@@ -11,23 +12,18 @@ import {MathOperations} from '../math.operations';
 
 export class CalculatorComponent implements OnInit {
 
-  operand1: number;
-  operand2: number;
-  selectedOperation: string;
+  model = new CalculationData();
   mathOperations = MathOperations;
   operationResult: OperationResult;
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {
-    if(this.mathOperations && this.mathOperations.length > 0){
-      this.selectedOperation = this.mathOperations[0];
-    }
   }
 
   recalculate() {
     this.apiService
-      .calculate(this.operand1, this.operand2, this.selectedOperation)
+      .calculate(this.model)
       .subscribe((res) => this.operationResult = res);
   }
 
