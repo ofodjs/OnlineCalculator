7e13f040a671f8e38d341cd703f5dd9bd88a8ca2
Sergey Serdyuk <serduksv@gmail.com>
2018-09-16 15:24:44 +0100

    Extracted API Url to environment config.

--- angular/src/app/api.service.ts
+++ angular/src/app/api.service.ts
@@ -2,18 +2,14 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {OperationResult} from './operationresult';
 import {Observable} from 'rxjs';
-import { HttpHeaders } from '@angular/common/http';
+import {HttpHeaders} from '@angular/common/http';
+import {environment} from '../environments/environment';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type':  'application/json'
-  })
-};
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private API_URL = 'http://localhost:4201/api';
+  private API_URL = environment.api_url;
 
   constructor(private http: HttpClient) {}
 
@@ -21,4 +17,4 @@ export class ApiService {
     return this.http
       .post<OperationResult>(`${this.API_URL}/calculate/`, requestData);
   }
-} 
+}
